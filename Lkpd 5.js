82dd42a7428d5622b4a2c03fa4cc3480c1bc18e2
andratyg <indraandra545@gmail.com>
2025-12-12 08:21:01 +0700

    LKPD

---
+++ Lkpd 5.js
@@ -0,0 +1,62 @@
+//for//
+//lkpd 5 no 1//
+  //           for(let i = 1; i <= 50; i++) {
+   //      console.log(i);
+   //  }
+   //lkpd 5 no 2
+    //        for(let i = 1; i <= 50; i++)
+    // if (i%2 != 0 ){console.log(i)}
+
+ //lkpd 5 no 3
+ //     for(let i = 1; i <= 50; i++)
+  //   if (i%2 == 0 ){console.log(i)}
+
+
+lkpd 5 no 4//
+    for(let i = 1; i <= 50; i++)
+   if (i%2 != 0 ){console.log("BILANGAN GANJIL "+i)}
+   else {
+       console.log("BILANGAN GENAP "+i)
+    }
+
+
+for (let i = 1; i<= 20 ; i++) {
+console.log( i + " Iqbal")
+}
+
+//while
+
+//lkpd 5 no 1//
+//	let i = 1
+//	while(i <= 50){
+//		 console.log(i);
+//		 i++;
+//	}
+	//lkpd 5 no 2//
+//let i = 1; 
+//while (i <= 50) {
+//    if (i % 2 != 0) {
+//        console.log(i); 
+//    }
+//    i++; 
+//}
+	//lkpd 5 no 3//
+//let i = 1; 
+//while (i <= 50) {
+ //   if (i % 2 == 0) {
+  //      console.log(i); 
+    //}
+    //i++; 
+//}
+	//lkpd 5 no 4//
+  let i = 1;
+while (i <= 50) {
+    if (i % 2 != 0) {
+        console.log("BILANGAN GANJIL " + i);
+    } else {
+        console.log("BILANGAN GENAP " + i);
+    }
+    i++;
+}
+
+
